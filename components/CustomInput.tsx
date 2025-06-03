dd4019c251a21e864e579fd6a28453d9fb7ebdcb
edilsonolegario <edilson_olegario@hotmail.com>
2025-06-03 11:19:18 -0300

    Forms structure on June 3th

---
+++ components/CustomInput.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { FormLabel, FormControl, FormField, FormMessage} from './ui/form'
+import { Input } from './ui/input'
+import { Control, FieldPath } from 'react-hook-form'
+import { authFormSchema } from '@/lib/utils'
+import { z } from 'zod'
+
+interface CustomInput {
+    control: Control<z.<typeof authFormSchema>>,
+    name: FieldPath<z.<typeof authFormSchema>>,
+    label: string,
+    placeholder: string
+}
+
+const CustomInput = ({control, name, label, placeholder}:CustomInput) => {
+    return (
+        <FormField
+            control={control}
+            name={name}
+            render={({ field }) => (
+                <div className='form-item'>
+                    <FormLabel className='form-label'>
+                        {label}
+                    </FormLabel>
+                    <div className="flex w-full flex-col">
+                        <FormControl>
+                            <Input
+                                placeholder={placeholder}
+                                className='input-class'
+                                type={name === 'password' ? 'password' : 'text'}
+                                {...field}
+                            />
+                        </FormControl>
+                        <FormMessage className='form-message mt-2'
+                        />
+                    </div>
+                </div>
+            )}
+        />
+    )
+}
+
+export default CustomInput
--- lib/utils.ts
+++ lib/utils.ts
@@ -195,6 +195,7 @@ export const getTransactionStatus = (date: Date) => {
   return date > twoDaysAgo ? "Processing" : "Success";
 };
 
+/*
 export const authFormSchema = (type: string) => z.object({
   // sign up
   firstName: type === 'sign-in' ? z.string().optional() : z.string().min(3),
@@ -208,4 +209,9 @@ export const authFormSchema = (type: string) => z.object({
   // both
   email: z.string().email(),
   password: z.string().min(8),
+})
+*/
+export const authFormSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(8)
 })
